612fd9ef8ac46c45321cdfe8f3c4e5dcfcb74caa
jeonghye-choi <ppllhm.0@gmail.com>
2020-09-08 04:59:17 +0900

    Fix database Client & winRatetest data(동우)
    원하는 정보만 가져오게 쳐리
    - Winning_rate필드만 가져옴
    - projection 사용

--- frontend/pages/api/winRatetest.js
+++ frontend/pages/api/winRatetest.js
@@ -6,25 +6,14 @@ const handler = nextConnect()
 handler.use(middleware)
 
 handler.get(async (req, res) => {
-  console.log(req)
-  console.log(req.db)
-  console.log(req.db.collection("Results"))
-  console.log(req.db.collection("Results").find({}, { Winning_rate: 1 }))
-  console.log(req.db.collection("Results").find({}, { Winning_rate: 1 }))
-  console.log(req.db.Results)
-  console.log(req.db.Results.find({}, { Winning_rate: 1 }))
-
+  console.log("ok")
+  var myReferenceToThis = this
   let doc = await req.db
     .collection("Results")
-    .find({}, { Winning_rate: 1, _id: 0 })
+    .find({}, { projection: { Winning_rate: 1, _id: 0 } })
+    .toArray()
+  {
+  }
   res.json(doc)
 })
 export default handler
-
-// export default (req, res) => {
-//   res.statusCode = 200
-//   res.json({
-//     win: 70,
-//     lose: 30
-//   })
-// }
