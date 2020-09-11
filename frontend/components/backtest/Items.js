6b666913be44b5ebed9ad145068b7b715376c9a9
jeonghye-choi <ppllhm.0@gmail.com>
2020-09-12 02:53:57 +0900

    Fix first time rendering error; backtest page
    백테스트페이지 종목부분이 처음 랜더링 되었을 때 값이 없어서 에러가 발생했다
    if else문으로 구분해줬다
    
    나중에 try 로 할 수 있는지 테스트해봐야 겠다

--- frontend/components/backtest/Items.js
+++ frontend/components/backtest/Items.js
@@ -6,21 +6,41 @@ const Items = () => {
   const dispatch = useDispatch()
   const stocksState = useSelector(state => state.stocksReducer)
   console.log("!!!!!!!!!!", stocksState)
-  console.log("길이!!!", stocksState.stocks.length)
-  console.log("첫번째 요소!!!", stocksState.stocks[0][0])
+  // console.log("길이!!!", stocksState.stocks[0].length)
+  // console.log("첫번째 요소!!!", stocksState.stocks[0][0])
 
-  const stocksDatas = stocksState.stocks[0]
-  const stocksDataList = stocksDatas.map(stocksData => <div>{stocksData}</div>)
+  const stocksDatas = stocksState.stocks
+  console.log("stocksDatas", stocksDatas)
+  let stocksDataList
 
-  const fetchData = () => {
-    dispatch(stocks_getData())
+  if (stocksDatas.length != 0) {
+    console.log("없다고 나옴!")
+    const revalancing_count = stocksState.stocks.length
+    console.log("리밸런싱 횟수", revalancing_count)
+
+    const stocks_count = stocksState.stocks[0]
+    console.log("첫번째 리밴런싱이 준 종목갯수:", stocks_count)
+
+    stocksDataList = stocksDatas[0].map(stocksData => <div>{stocksData}</div>)
+
+    // for (var i = 0; i < revalancing_count; i++) {
+    //   stocksDataList = stocksDatas[i].map(stocksData => <div>{stocksData}</div>)
+    // }
+  } else {
+    //없을 때
+    stocksDataList = ""
   }
-  console.log(stocksState)
+
+  // const fetchData = () => {
+  //   dispatch(stocks_getData())
+  // }
+  // console.log(stocksState)
 
   return (
     <>
       <div>{stocksState.loading}</div>
       <div className="items-stockList">{stocksDataList}</div>
+      {/* <div className="items-stockList"></div> */}
     </>
   )
 }
